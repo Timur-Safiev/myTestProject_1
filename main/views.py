ca6d5f10f19a471382e8b71726888886cd6ade21
Timur Safiev <tsafiev285@gnail.com>
2024-12-07 20:15:37 +0300

    created a templates folder in the main folder, added two functions in the Sushi_Osaka
    created a templates folder in the main folder, added two functions index, about, registered links in the Sushi_Osaka folder, created links to the home page, about us page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Суши Осака',
+        'content': 'Добро пожаловать на сайт Суши Осака'
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Суши Осака - О нас',
+        'content':'О нас',
+        'text_on_page': 'Текст какие мы класные'
+    }
+
+    return render(request, 'main/about.html', context)
